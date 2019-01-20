9ebd8bf3f370cb54b7165787b840056f57d9d579
fattouche <alex.fattouche@gmail.com>
2019-01-20 13:07:02 -0800

    Rename app

--- images/django/entrypoint.sh
+++ images/django/entrypoint.sh
@@ -3,4 +3,6 @@ bash /service/images/wait-for-it.sh db:3306 --timeout=300
 
 bash /service/images/wait-for-it.sh cache:11211 --timeout=300
 
+python /service/day_trader/manage.py migrate
+
 exec "$@"
--- images/worker/entrypoint.sh
+++ images/worker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bash /service/images/wait-for-it.sh queue:6379 --timeout=300
+bash ./wait-for-it.sh queue:6379 --timeout=300
 
 exec "$@"
