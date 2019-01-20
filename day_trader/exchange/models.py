a7435c74a5e8b36c5e01fedf18fe5524c04d0b35
fattouche <alex.fattouche@gmail.com>
2019-01-20 14:38:01 -0800

    Migrations and db setup

--- day_trader/exchange/models.py
+++ day_trader/exchange/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length=3, primary_key=True)
+    price = models.PositiveIntegerField(default=0)
+
+
+class User(models.Model):
+    user_id = models.CharField(max_length=100, primary_key=True)
+    balance = models.PositiveIntegerField(default=0)
+    stocks = models.ManyToManyField(Stock)
+
+
+class Trigger(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField(default=0)
+    sell_amount = models.PositiveIntegerField(default=0)
+    trigger_type = models.TextField(default="buy")
+
+
+class Sell(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock_symbol = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    cash_amount = models.PositiveIntegerField(default=0)
+    stock_sold_amount = models.PositiveIntegerField(default=0)
+    commited = models.BooleanField(default=False)
+
+
+class Buy(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock_symbol = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    cash_amount = models.PositiveIntegerField(default=0)
+    commited = models.BooleanField(default=False)
--- images/django/entrypoint.sh
+++ images/django/entrypoint.sh
@@ -3,6 +3,7 @@ bash /service/images/wait-for-it.sh db:3306 --timeout=300
 
 bash /service/images/wait-for-it.sh cache:11211 --timeout=300
 
+python /service/day_trader/manage.py makemigrations
 python /service/day_trader/manage.py migrate
 
 exec "$@"
