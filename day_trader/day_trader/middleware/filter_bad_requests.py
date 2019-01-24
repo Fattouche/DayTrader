57133a2011a5e1074441e1e4bc21102bc1ea8a93
Isaac Sahle <isaacwsahle@gmail.com>
2019-01-24 15:16:32 -0800

    Filter out negative amounts & invalid stock symbols.

--- day_trader/day_trader/middleware/filter_bad_requests.py
+++ day_trader/day_trader/middleware/filter_bad_requests.py
@@ -1,9 +1,15 @@
+from django.http import HttpResponseBadRequest
+from decimal import Decimal
+import re
 class FilterBadRequestsMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
     
-    def __call__(self, request):        
-         # Stuff to do before a request. 
-         response = self.get_response(request)
-         # Stuff to do after a request.
-         return response
+    def __call__(self, request):
+        queryDict = request.POST if request.method == 'POST' else request.GET
+        if 'amount' in queryDict and Decimal(queryDict['amount']) < 0:
+            return HttpResponseBadRequest("Amount should be positive")
+        if 'symbol' in queryDict and (not queryDict['symbol'] or len(queryDict['symbol']) > 3 or not queryDict['symbol'].isalpha()):
+            return HttpResponseBadRequest("Symbol should be 1-3 alphabetic characters")
+
+        return self.get_response(request)
