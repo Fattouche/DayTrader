8246288c38c6b6d44c3a415878a285c30f274308
Isaac Sahle <isaacwsahle@gmail.com>
2019-01-24 13:21:41 -0800

    Initial hooks for middleware.

--- day_trader/day_trader/settings.py
+++ day_trader/day_trader/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'day_trader.middleware.filter_bad_requests.FilterBadRequestsMiddleware',
 ]
 
 ROOT_URLCONF = 'day_trader.urls'
--- images/django/entrypoint.sh
+++ images/django/entrypoint.sh
@@ -4,7 +4,7 @@ bash /startup/wait-for-it.sh db:3306 --timeout=300
 
 bash /startup/wait-for-it.sh cache:11211 --timeout=300
 
-python ./day_trader/manage.py makemigrations
-python ./day_trader/manage.py migrate
+python ./manage.py makemigrations
+python ./manage.py migrate
 
 exec "$@"
