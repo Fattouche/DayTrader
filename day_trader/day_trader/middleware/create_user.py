6be79264708ae5b02e9f47b33c8247cf519653a3
Isaac Sahle <isaacwsahle@gmail.com>
2019-01-26 16:15:40 -0800

    Added more filtering rules, and minor refactors.

--- day_trader/day_trader/middleware/create_user.py
+++ day_trader/day_trader/middleware/create_user.py
@@ -7,12 +7,11 @@ class CreateUserMiddleware(object):
     
     def __call__(self, request):
         query_dict = request.POST if request.method == 'POST' else request.GET
-        
+
         if 'user_id' in query_dict:
-            try:
-                u_id = query_dict['user_id']
-                user = User.objects.get(user_id=u_id)
-            except ObjectDoesNotExist:
-                User.objects.create(user_id=u_id, balance=0)
-        
+            User.objects.get_or_create(
+                user_id=query_dict['user_id'],
+                defaults={'balance': 0},
+            )
+
         return self.get_response(request)
--- day_trader/day_trader/middleware/filter_bad_requests.py
+++ day_trader/day_trader/middleware/filter_bad_requests.py
@@ -1,19 +1,42 @@
-from django.http import HttpResponseBadRequest
+from django.http import JsonResponse
 from django.urls import reverse
 from decimal import Decimal
 
 class FilterBadRequestsMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
+        self.index = reverse('index')
+        self.admin_index = reverse('admin:index')
+        self.paths_requiring_symbol_and_amount = {
+            reverse('buy'),
+            reverse('sell'),
+            reverse('set_buy_amount'),
+            reverse('set_buy_trigger'),
+            reverse('set_sell_amount'),
+            reverse('set_sell_trigger'),
+        }
+        self.add_path = reverse('add')
+        self.quote_path = reverse('quote')
+        self.dumplog_path = reverse('dumplog')
     
     def __call__(self, request):
         query_dict = request.POST if request.method == 'POST' else request.GET
         
-        if 'amount' in query_dict and Decimal(query_dict['amount']) < 0:
-            return HttpResponseBadRequest('Amount should be positive')
-        if 'symbol' in query_dict and (not query_dict['symbol'] or len(query_dict['symbol']) > 3 or not query_dict['symbol'].isalpha()):
-            return HttpResponseBadRequest('Symbol should be 1-3 alphabetic characters') 
-        if 'user_id' not in query_dict and request.path != reverse('index') and not request.path.startswith(reverse('admin:index')):
-            return HttpResponseBadRequest('User id is required')
-        
+        if 'user_id' not in query_dict and request.path != self.index and not request.path.startswith(self.admin_index):
+            return JsonResponse({'error': 'User id is required'}, status=400)
+        hasSymbol = 'symbol' in query_dict
+        hasAmount = 'amount' in query_dict
+        if not (hasSymbol and hasAmount) and request.path in self.paths_requiring_symbol_and_amount:
+            return JsonResponse({'error': 'Symbol and amount is required'}, status=400)
+        if not hasSymbol and request.path == self.quote_path:
+            return JsonResponse({'error': 'Symbol is required'}, status=400)
+        if not hasAmount and request.path == self.add_path:
+            return JsonResponse({'error': 'Amount is required'}, status=400)
+        if hasAmount and Decimal(query_dict['amount']) < 0:
+            return JsonResponse({'error': 'Amount should be positive'}, status=400)
+        if hasSymbol and (not query_dict['symbol'] or len(query_dict['symbol']) > 3 or not query_dict['symbol'].isalpha()):
+            return JsonResponse({'error': 'Symbol should be 1-3 alphabetic characters'}, status=400)
+        if 'filename' not in query_dict and request.path == self.dumplog_path:
+            return JsonResponse({'error': 'Filename is required'}, status=400)
+
         return self.get_response(request)
