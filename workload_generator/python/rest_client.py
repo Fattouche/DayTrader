2b96c4578e0b744120dc5ae6fe83dc083b1624fa
fattouche <alex.fattouche@gmail.com>
2019-01-28 17:45:57 -0800

    Adding golang and fixing params on GET requests

--- workload_generator/python/rest_client.py
+++ workload_generator/python/rest_client.py
@@ -1,30 +1,31 @@
 import requests
-import json 
+import json
+
 
 class RestClient:
 
-    get_cmds = {"QUOTE":1, "DUMPLOG":1, "DISPLAY_SUMMARY":1}
+    get_cmds = {"QUOTE": 1, "DUMPLOG": 1, "DISPLAY_SUMMARY": 1}
 
     symbol_cmds = {
-        "QUOTE":1,
-        "BUY":1,
-        "SELL":1, 
-        "SET_BUY_AMOUNT":1, 
-        "CANCEL_SET_BUY":1, 
-        "SET_BUY_TRIGGER":1, 
-        "SET_SELL_AMOUNT":1, 
-        "SET_SELL_TRIGGER":1, 
-        "CANCEL_SET_SELL":1
+        "QUOTE": 1,
+        "BUY": 1,
+        "SELL": 1,
+        "SET_BUY_AMOUNT": 1,
+        "CANCEL_SET_BUY": 1,
+        "SET_BUY_TRIGGER": 1,
+        "SET_SELL_AMOUNT": 1,
+        "SET_SELL_TRIGGER": 1,
+        "CANCEL_SET_SELL": 1
     }
 
     amount_cmds = {
-    "ADD":1,
-    "BUY":1,
-    "SELL":1,
-    "SET_BUY_AMOUNT":1,
-    "SET_BUY_TRIGGER":1,
-    "SET_SELL_AMOUNT":1,
-    "SET_SELL_TRIGGER":1,
+        "ADD": 1,
+        "BUY": 1,
+        "SELL": 1,
+        "SET_BUY_AMOUNT": 1,
+        "SET_BUY_TRIGGER": 1,
+        "SET_SELL_AMOUNT": 1,
+        "SET_SELL_TRIGGER": 1,
     }
 
     def __init__(self, endpoint):
@@ -34,8 +35,8 @@ class RestClient:
 
         cmd_type, req_data = self.build_request(user_id, *cmd.split(" "))
         url = self.build_url(self.endpoint, cmd_type)
-        
-        r = requests.get(url, data=json.dumps(req_data)) if cmd_type in  self.get_cmds else requests.post(url, data=json.dumps(req_data))
+        r = requests.get(url, params=req_data) if cmd_type in self.get_cmds else requests.post(
+            url, data=json.dumps(req_data))
 
     def build_url(self, endpoint, cmd_type):
         url = "{}{}{}".format(endpoint, "/", cmd_type.lower())
@@ -43,27 +44,26 @@ class RestClient:
 
     def build_request(self, user_id, *args):
         data = {
-            "user_id": None, 
+            "user_id": None,
             "symbol": None,
             "amount": None,
             "filename": None
         }
 
-        cmd_type =  args[0]
+        cmd_type = args[0]
 
         data["user_id"] = user_id if user_id != "adminxxx" else None
-        
+
         if cmd_type == "DUMPLOG":
             data["filename"] = args[2] if len(args) > 2 else args[1]
 
-        if cmd_type in  self.symbol_cmds:
+        if cmd_type in self.symbol_cmds:
             data["symbol"] = args[2] if len(args) > 2 else None
-        
-        if cmd_type in  self.amount_cmds:
+
+        if cmd_type in self.amount_cmds:
             if cmd_type == "ADD":
                 data["amount"] = float(args[2]) if len(args) > 2 else None
             else:
                 data["amount"] = float(args[3]) if len(args) > 3 else None
 
         return cmd_type, data
-        
