8e0cf0bbc2ad7d45059764ff765452177884565c
Isaac Sahle <isaacwsahle@gmail.com>
2019-02-18 16:29:44 -0800

    Use material-ui sign in component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Loginscreen from './Loginscreen';
+import SignIn from './SignIn';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      loginPage:[],
-      uploadScreen:[]
-    }
-  }
-  
-  componentWillMount(){
-    var loginPage =[];
-    loginPage.push(<Loginscreen parentContext={this}/>);
-    this.setState({
-                  loginPage:loginPage
-                    })
-  }
-  
   render() {
     return (
-      <div className='App'>
-        {this.state.loginPage}
-        {this.state.uploadScreen}
-      </div>
+      <div className='App'><SignIn parentContext={this}/></div>
     );
   }
 }
