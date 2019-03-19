d389a8c41ca5609c59082cc85eaf1eaa4cd4adbb
Cailan St Martin <cailanstm@gmail.com>
2019-03-19 00:55:01 -0400

    lab daytrader lb

--- golang/day_trader/images/envoy/entrypoint.sh
+++ golang/day_trader/images/envoy/entrypoint.sh
@@ -2,11 +2,9 @@
 
 cd /service
 
-bash ./wait-for-it.sh daytrader_web:41000 --timeout=300
-
 #Uncomment to scale up
-# bash ./wait-for-it.sh day_trader_daytrader_web_1:41000 --timeout=300
-# bash ./wait-for-it.sh day_trader_daytrader_web_2:41000 --timeout=300
-# bash ./wait-for-it.sh day_trader_daytrader_web_3:41000 --timeout=300
+ bash ./wait-for-it.sh 192.168.1.157:41000 --timeout=300
+ bash ./wait-for-it.sh 192.168.1.194:41000 --timeout=300
+ bash ./wait-for-it.sh 192.168.1.191:41000 --timeout=300
 
 /usr/local/bin/envoy -c /etc/envoy/envoy.yaml
