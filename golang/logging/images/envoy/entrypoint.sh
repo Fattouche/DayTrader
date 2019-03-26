23f302c82c821c917631bc0724b8d2a97ed96f30
Cailan St Martin <cailanstm@gmail.com>
2019-03-26 15:08:01 -0400

    push updated IPs

--- golang/logging/images/envoy/entrypoint.sh
+++ golang/logging/images/envoy/entrypoint.sh
@@ -2,11 +2,13 @@
 
 cd /service
 
-bash ./wait-for-it.sh ${LOGGING_SERVER_IP}:${LOGGING_SERVER_PORT} --timeout=300
+# bash ./wait-for-it.sh ${LOGGING_SERVER_IP}:${LOGGING_SERVER_PORT} --timeout=300
+bash ./wait-for-it.sh 192.168.1.240:40000 --timeout=300
+bash ./wait-for-it.sh 192.168.1.228:40000 --timeout=300
 
 #Uncomment to scale up
 #bash ./wait-for-it.sh logging_1:40000 --timeout=300
 #bash ./wait-for-it.sh logging_2:40000 --timeout=300
 #bash ./wait-for-it.sh logging_3:40000 --timeout=300
 
-/usr/local/bin/envoy -c /etc/envoy/envoy.yaml
+/usr/local/bin/envoy -c /etc/envoy/envoy.yaml --log-level info
--- golang/logging/images/nginx/entrypoint.sh
+++ golang/logging/images/nginx/entrypoint.sh
@@ -2,7 +2,9 @@
 
 cd /service
 
-bash ./wait-for-it.sh logging_server:40000 --timeout=300
+# bash ./wait-for-it.sh ${LOGGING_SERVER_IP}:${LOGGING_SERVER_PORT} --timeout=300
+bash ./wait-for-it.sh 192.168.1.240:40000 --timeout=300
+bash ./wait-for-it.sh 192.168.1.187:40000 --timeout=300
 
 #Uncomment to scale up
 #bash ./wait-for-it.sh logging_1:40000 --timeout=300
